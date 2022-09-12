de5d7649ea23daae9f0c45e0bc148a10e5df4acc
John Loane <john.loane@dkit.ie>
2022-09-12 12:53:37 +0100

    Showing GD2 23 how to use git
    Getting started with primitive types

---
+++ src/main/java/com/dkit/gd2/johnloane/App.java
@@ -0,0 +1,17 @@
+package com.dkit.gd2.johnloane;
+
+/**
+ * GD2 Primitive Types
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int myValue = 10000;
+
+        System.out.println(myValue);
+
+        
+    }
+}
